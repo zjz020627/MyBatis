fe361afdaeb5c9294b6d25c12fc77ec89a1b97af
Eternallove7 <614056287@qq.com>
2022-05-17 22:44:13 +0800

    day01

--- MyBatis_demo1/src/main/java/com/study/mybatis/mapper/UserMapper.java
+++ MyBatis_demo1/src/main/java/com/study/mybatis/mapper/UserMapper.java
@@ -1,5 +1,44 @@
-package com.study.mybatis.mapper;/**
-@author RenAshbell
-@create 2022-05-17-20:19
-*/public interface UserMapper {
+package com.study.mybatis.mapper;
+
+import com.study.mybatis.pojo.User;
+
+import java.util.List;
+
+/**
+ * @author RenAshbell
+ * @create 2022-05-17-20:19
+ */
+public interface UserMapper {
+    /**
+     * Mybatis面向接口编程的两个一致
+     * 1.映射文件的namespace要和mapper接口的全类目保持一致
+     *
+     * 表--实体类--mapper接口--映射文件
+     */
+
+    /**
+     *
+     * 增加用户信息
+     */
+    int insertUser();
+
+    /**
+     * 修改用户信息
+     */
+    void updateUser();
+
+    /**
+     * 删除用户信息
+     */
+    void deleteUser();
+
+    /**
+     * 根据id查询用户信息
+     */
+    User getUserById();
+
+    /**
+     * 查询所有的用户信息
+     */
+    List<User> getAllUser();
 }
--- MyBatis_demo1/src/test/java/com/study/mybatis/test/MyBatisTest.java
+++ MyBatis_demo1/src/test/java/com/study/mybatis/test/MyBatisTest.java
@@ -1,5 +1,59 @@
-package com.study.mybatis.test;/**
-@author RenAshbell
-@create 2022-05-17-20:36
-*/public class MyBatisTest {
+package com.study.mybatis.test;
+
+import com.study.mybatis.mapper.UserMapper;
+import com.study.mybatis.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author RenAshbell
+ * @create 2022-05-17-20:36
+ */
+public class MyBatisTest {
+
+    /**
+     * sqlSession默认不自动提交事务，若要自动提交事务
+     * 可以使用sqlSessionFactory.openSession(true);
+     */
+
+    @Test
+    public void testMyBatis() throws IOException {
+        // 加载核心配置文件
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        // 获取sqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 获取sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        // 获取sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        // 获取mapper接口对象
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        // 测试功能
+        int result = mapper.insertUser();
+        // 提交事务
+//        sqlSession.commit();
+        System.out.println("result:" + result);
+
+    }
+
+    @Test
+    public void testCRUD() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+//        mapper.updateUser();
+//        mapper.deleteUser();
+//        User user = mapper.getUserById();
+        List<User> list = mapper.getAllUser();
+        list.forEach(user -> System.out.println(user));
+    }
 }
